9569302e63b6b71aae4268d01946368aaabe96fa
dstagnaro1 <danielstagnaro@gmail.com>
2016-06-11 16:26:52 -0700

    this shows the use of view controller and tab bar controllers working at the same time

---
+++ Navigating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigating
+//
+//  Created by Daniel Stagnaro on 6/11/16.
+//  Copyright © 2016 Daniel Stagnaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
